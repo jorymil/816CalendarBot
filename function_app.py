8e05163c3ebd5836228df71e4502bb28e26eb9fd
David Klingler <deweydbb@gmail.com>
2025-03-09 09:33:37 -0500

    changed to run at 10am cdt

--- function_app.py
+++ function_app.py
@@ -7,7 +7,7 @@ from calender_bot.calender_bot import send_slack_messages
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 
-@app.timer_trigger(schedule="0 0 9 * * *", arg_name="myTimer", run_on_startup=False, use_monitor=False) 
+@app.timer_trigger(schedule="0 0 15 * * *", arg_name="myTimer", run_on_startup=False, use_monitor=False) 
 def calender_bot(myTimer: func.TimerRequest) -> None:
     logging.info('Python timer trigger function executed.')
     send_slack_messages()
